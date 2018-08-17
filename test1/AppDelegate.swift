66feea22ebfe75b7e2aa838e2aeed48d6ecc4088
hoo <hoo@hooui-MacBook-Pro.local>
2018-08-17 16:22:49 +0900

    first commit

--- test1/AppDelegate.swift
+++ test1/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        var a : NSString = "1111"
+        print(a)
         return true
     }
 
